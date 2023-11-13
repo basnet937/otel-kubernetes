0b70262520d8e31a4f997f895766dda62fdb02c2
muzammil360 <muzammil360@gmail.com>
2023-11-13 16:38:45 +0500

    added sample app

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # otel-kubernetes
+
+
+## Docker 
+
+```
+docker build -t my-app:1.0 .       
+
+```
---
+++ app/server.js
@@ -0,0 +1,24 @@
+let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+app.get('/ping', function(req, res){
+  res.send({'ping': 'pong'})
+})
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
+
